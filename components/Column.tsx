fc4adce90fbb2c53f6a51096f65c2a6b55c1b466
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-09-11 10:36:00 +0530

    bug:bug removed

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
-import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { useBoardStore } from "@/store/BoardStore";
+// import { useModalStore } from "@/store/ModalStore";
 
 type Props = {
   id: TypedColumn;
@@ -18,6 +19,9 @@ const idToColumnText: {
 };
 
 function Column({ id, todos, index }: Props) {
+  // const { openModal } = useModalStore()
+
+  // const [searchString] = useBoardStore((state) => [state.searchString]);
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -26,47 +30,58 @@ function Column({ id, todos, index }: Props) {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          <Droppable droppableId={id} type="card">
-            {(provided, snapShot) => (
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
               <div
                 {...provided.droppableProps}
                 ref={provided.innerRef}
                 className={`p-2 rounded-2xl shadow-sm ${
-                  snapShot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
                 }`}
               >
-                <h2 className="flex justify-between font-bold text-xl p-2">
-                  {idToColumnText[id]}{" "}
-                  <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
-                    {todos.length}
-                  </span>
+                <h2 className="flex justify-between font-bold text-xl p-2 py-1">
+                  {idToColumnText[id]}
+                  {/* <span className="text-gray-500 bg-gray-200 rounded-full font-normal px-2 py-1">
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) => todo.title.toLowerCase().includes(searchString.toLowerCase())).length}
+                  </span> */}
                 </h2>
-
                 <div className="space-y-2">
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.$id}
-                      draggableId={todo.$id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <TodoCard
-                          todo={todo}
-                          index={index}
-                          id={id}
-                          innerRef={provided.innerRef}
-                          draggableProps={provided.draggableProps}
-                          dragHandleProps={provided.dragHandleProps}
-                        ></TodoCard>
-                      )}
-                    </Draggable>
-                  ))}
+                  {todos.map((todo, index) => {
+                    if (
+                      // searchString &&
+                      !todo.title.toLowerCase()
+                      // .includes(searchString.toLowerCase())
+                    )
+                      return null;
 
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <TodoCard
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            draggableProps={provided.draggableProps}
+                            dragHandleProps={provided.dragHandleProps}
+                          />
+                        )}
+                      </Draggable>
+                    );
+                  })}
                   {provided.placeholder}
-
                   <div className="flex items-end justify-end p-2">
                     <button className="text-green-500 hover:text-green-600">
-                      <PlusCircleIcon className="h-10 w-10" />
+                      <PlusCircleIcon
+                        // onClick={openModal}
+                        className="h-10 w-10"
+                      />
                     </button>
                   </div>
                 </div>
